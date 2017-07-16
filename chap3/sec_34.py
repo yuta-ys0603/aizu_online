9c5895d52223af2f4a6940f5301ddbb92a1bfa2d
下田雄太 <mti.internship.e9@gmail.com>
2017-07-16 14:32:14 +0900

    finished sec_34.py

--- chap3/sec_34.py
+++ chap3/sec_34.py
@@ -2,8 +2,19 @@ count = int(input())
 num_list = [int(i) for i in input().split()]
 loop_count = 0
 
-for i in range(0,count)
+for i in range(0,count):
+    min_j = i
+
+    for j in range(i,count):
+        if num_list[j] < num_list[min_j]:
+            min_j = j
+
+    if num_list[min_j] < num_list[i]:
+        v = num_list[min_j]
+        num_list[min_j] = num_list[i]
+        num_list[i] = v
+        loop_count += 1
 
 maped_list = map(str, num_list)
 print(' '.join(maped_list))
-print(count)
+print(loop_count)
