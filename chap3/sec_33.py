5cc08df32493ea8e4d06e9f762e1aed48398921b
下田雄太 <mti.internship.e9@gmail.com>
2017-07-16 14:11:54 +0900

    touch sec_33.py

---
+++ chap3/sec_33.py
@@ -0,0 +1,20 @@
+count = int(input())
+num_list = [int(i) for i in input().split()]
+flag = 1
+count = 0
+
+while flag:
+    flag = 0
+
+    for i in reversed(range(1,len(num_list))):
+        if num_list[i] < num_list[i - 1]:
+            v = num_list[i]
+            num_list[i] = num_list[i - 1]
+            num_list[i - 1] = v
+
+            flag = 1
+            count += 1
+
+maped_list = map(str, num_list)
+print(' '.join(maped_list))
+print(count)
