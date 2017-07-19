9294ccad4bc28825063fa119a12a47c5b4116520
下田雄太 <mti.internship.e9@gmail.com>
2017-07-19 17:18:41 +0900

    finished sec_43.py

---
+++ chap4/sec_43.py
@@ -0,0 +1,20 @@
+count, qms = map(int, input().split())
+process_list = [[i for i in input().split()] for j in range(count)]
+finished_list = []
+time = 0
+
+
+while len(process_list) != 0:
+    # qmsよりもプロセスが長いとき
+    if int(process_list[0][1]) - qms > 0:
+        # qmsを引いて挿入し直す
+        process_list[0][1] = int(process_list[0][1]) - qms
+        process_list.append(process_list.pop(0))
+        time = time + qms
+    else:
+        time = int(process_list[0][1]) + time
+        process_list[0][1] = time
+        finished_list.append(process_list.pop(0))
+
+for i in range(count):
+    print(' '.join(map(str,finished_list[i])))
