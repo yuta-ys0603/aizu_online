eeeaa226345fc811d13adca81bc6a3897fd0a307
下田雄太 <mti.internship.e9@gmail.com>
2017-07-24 01:56:04 +0900

    sec_54.py not accepted

---
+++ chap5/sec_54.py
@@ -0,0 +1,12 @@
+n = int(input())
+dic = []
+
+for i in range(n):
+    com, num = input().split()
+    if com == 'insert':
+        dic.append(num)
+    elif com == 'find':
+        if num in dic:
+            print('yes')
+        else:
+            print('no')
