3435bab3d6bca8d0519c78e4258a97379b8c39c6
下田雄太 <mti.internship.e9@gmail.com>
2017-07-24 18:03:38 +0900

    accepted sec_53.py

--- chap5/sec_53.py
+++ chap5/sec_53.py
@@ -3,20 +3,37 @@ S = [int(i) for i in input().split()]
 q = int(input())
 T = [int(i) for i in input().split()]
 count = 0
+left = 0
+right = len(S)
 
 for i in T:
-    mid = int(len(S) / 2)
-    copy_list = S[:]
+    left = 0
+    right = len(S)
 
-    while len(copy_list) > 0:
-        if i == copy_list[mid]:
+    while right > left:
+        mid = int((left + right)/2)
+
+        if i == S[mid]:
             count += 1
             break
-        elif i >= copy_list[mid]:
-            copy_list = copy_list[mid+1:]
+        elif i >= S[mid]:
+            left = mid + 1
         else:
-            copy_list = copy_list[:mid]
-
-        mid = int(len(copy_list) / 2)
+            right = mid
 
 print(count)
+# cp_list = S[:]
+# for i in T:
+#     mid = int(len(S) / 2)
+#     cp_list = S[:]
+#
+#     while len(cp_list) > 0:
+#         if i == cp_list[mid]:
+#             count += 1
+#             break
+#         elif i >= cp_list[mid]:
+#             cp_list = cp_list[mid+1:]
+#         else:
+#             cp_list = cp_list[:mid]
+#
+#         mid = int(len(cp_list) / 2)
